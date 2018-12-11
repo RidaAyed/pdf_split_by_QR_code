6b30c74f3c70828a95ce899896b288afb68d8c25
Aleksey Molchanov <molchanovav@gmail.com>
2018-12-12 00:27:39 +0300

    debug

--- tool.py
+++ tool.py
@@ -44,7 +44,7 @@ class Tool(object):
         pil = Image.open(file_path)
         # pil = im.convert('L')
         width, height = pil.size
-        print ('pil.size', file_path, pil.size, len(open(file_path,'rb').read()))
+        print ('pil.size', file_path, pil.mode, pil.size, len(open(file_path,'rb').read()))
 
         try:
             raw = pil.tobytes()
